ccbbc44ce854ca20be998f5eaaa2ed696cfaf5ed
Andrey Leal <36980562+alsoza94@users.noreply.github.com>
2018-03-06 12:46:38 -0600

    procesar_registro
    Actualizacion procesar registro

--- procesar_registro.php
+++ procesar_registro.php
@@ -3,56 +3,67 @@ include('conexion.php');
 /*Si presiono el submit inicia el proceso de registro*/
 if (isset($_POST['registrarse']))
 {
+	 $nombre=$_POST['registro_nombre'];
+	 $apellidos=$_POST['registro_apellidos'];
+	 $email=$_POST['registro_correo'];
+
 	 $usuario=$_POST['registro_usuario'];
 	 $contrasena=$_POST['registro_contrasena'];
 	 $contraseña_confirmar=$_POST['confirmar_contrasena'];
 	 $privilegio=$_POST['permisos'];
 
+	 /*Encripto datos del usuario al momento de insertarse en la BD con password_hash*/
+	 $nombre_cifrado = password_hash($nombre,PASSWORD_DEFAULT);
+	 $apellido_cifrado = password_hash($apellidos,PASSWORD_DEFAULT);
+	 $correo_cifrado = password_hash($email,PASSWORD_DEFAULT);
+	 $contrasena_cifrada = password_hash($contrasena,PASSWORD_DEFAULT);
+	 /*---------------------------------Fin cifrado---------------------------------*/
+
+
 	 /*Valido que ninguna de las variables este vacia, a pesar de que el mismo formulario ya valida que no vengan vacias*/
 	 if(empty($_POST['registro_usuario']))
 	 {
-		 echo "<script>alert('Vacio');</script>";
+		 echo "<script> alert('¡El campo de nombre de usuario está vacio!');window.location= 'registro.php' </script>";
 		 return;
 	 }
 	 else if(empty($_POST['registro_contrasena']))
 	 {
-		 echo "<script>alert('Vacio');</script>";
+		 echo "<script> alert('¡El campo de contraseña está vacio!');window.location= 'registro.php' </script>";
 		 return;
 	 }
 	 else if(empty($_POST['confirmar_contrasena']))
 	 {
-		 echo "<script>alert('Vacio');</script>";
+		 echo "<script> alert('¡El campo de confirmar contraseña está vacio!');window.location= 'registro.php' </script>";
 		 return;
 	 }
+	 else if(!filter_var($_POST['registro_correo'] , FILTER_VALIDATE_EMAIL))
+	 {
+		  echo "<script> alert('¡El correo electronico ingresado no es valido!');window.location= 'registro.php' </script>";
+		  return;
+	 }
+	 /*------------------------------Fin validacion campos de ingreso--------------------------------------------------------*/
 	 else
 	 {
-		 /*Query para averiguar si nombre de usuario ya existe, si existe lanza mensaje de error*/
-		$query="SELECT * FROM usuario where usuario='$usuario'";
-		$stmt = $conexion->query($query);
-		if ($stmt->rowCount()>0)
+		/*Query para averiguar si nombre de usuario ya existe, si existe lanza mensaje de error*/
+		$queryuser="SELECT * FROM usuario where usuario='$usuario'";
+		$stmtuser = $conexion->query($queryuser);
+		$stmtuser->execute();
+		if($stmtuser->rowCount()>0)
 		{
-
-			$mensaje='<div class="alert alert-danger alert-dismissable col-md-offset-4 col-md-3 text-center">
-			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-			Nombre de <strong>Usuario y/o Email</strong> ya existente</div>';
-			return;
-
+			 echo "<script> alert('¡El nombre de usuario ya está en uso, ingrese uno nuevo!');window.location= 'registro.php' </script>";
+			 return;
 		}
 		/*Averiguo si la contraseña y confirmar cntraseña son identicas*/
 		else if ($contrasena!=$contraseña_confirmar)
 		{
-			$mensaje='<div class="alert alert-danger alert-dismissable col-md-offset-4 col-md-3 text-center">
-			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-			Las contraseñas <strong>no coinciden</strong></div>';
-			return;
+			 echo "<script> alert('¡Las contraseñas no son identicas, verifique!');window.location= 'registro.php' </script>";
+			 return;
 
 		}
 		/*Valido que el privilegio no sea igual a 0*/
 		else if($privilegio == 0 )
 		{
-			$mensaje='<div class="alert alert-danger alert-dismissable col-md-offset-4 col-md-3 text-center">
-			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-			Las contraseñas <strong>no coinciden</strong></div>';
+			echo "<script> alert('¡Verifique que eligio un tipo de permiso!');window.location= 'registro.php' </script>";
 			return;
 
 		}
@@ -61,32 +72,48 @@ if (isset($_POST['registrarse']))
 		{
 			try
 			{
-				/*Encripto la contraseña del usuario al momento de insertarse en la BD con password_hash*/
-				$contrasena_cifrada = password_hash($contrasena,PASSWORD_DEFAULT);
+				/*Query para averiguar si el correo ya existe, si existe lanza mensaje de error*/
+				$querycorreo="SELECT * FROM persona where email='$correo_cifrado'";
+				$stmtcorreo = $conexion->query($querycorreo);
+				$stmtcorreo->execute();
+				if ($stmtcorreo->rowCount()>0)
+				{
+					echo "<script> alert('¡El correo electronico ya está en uso, ingrese uno nuevo!');window.location= 'registro.php' </script>";
+					return;
+				}
+				else
+				{
+				/*Primero hago el insert en la tabla persona de los datos del usuario*/
+				$query="INSERT INTO persona (nombre, apellidos, email) VALUES (:nombre, :apellidos, :email)";
+				$nuevoPersona = $conexion -> prepare($query);
+				$nuevoPersona -> bindParam(':nombre'   , $nombre_cifrado, PDO::PARAM_STR);
+				$nuevoPersona -> bindParam(':apellidos', $apellido_cifrado, PDO::PARAM_STR);
+				$nuevoPersona -> bindParam(':email', $correo_cifrado, PDO::PARAM_STR);
+				$stmt= $nuevoPersona -> execute();
 
-				$query="INSERT INTO usuario (usuario, contrasena, privilegio) VALUES (:usuario, :contrasena, :privilegio)";
+				/*Una vez que guardo los datos del usuario en la tabla persona, hago otro insert pero en la tabla usuario
+				donde guardo el nombre de usuario y la contraseña, junto con el privilegio. Ademas dentro del insert hago
+				una consulta donde obtengo el id de la persona de acuerdo al nombre que ingrese anteriormente, esto porque
+				hay una llave foranea*/
+
+				$query="INSERT INTO usuario (usuario, contrasena, privilegio, id_persona)
+				VALUES (:usuario, :contrasena, :privilegio, (Select id from persona where nombre= :nombre))";
 				$nuevoUsuario = $conexion -> prepare($query);
 				$nuevoUsuario -> bindParam(':usuario'   , $usuario, PDO::PARAM_STR);
 				$nuevoUsuario -> bindParam(':contrasena', $contrasena_cifrada, PDO::PARAM_STR);
 				$nuevoUsuario -> bindParam(':privilegio', $privilegio, PDO::PARAM_STR);
-				$stmt= $nuevoUsuario -> execute();
+				$nuevoUsuario -> bindParam(':nombre',     $nombre_cifrado, PDO::PARAM_STR);
+				$stmt1= $nuevoUsuario -> execute();
+				}
 			}
 			catch (PDOException $error)
 			{
-				echo $error;
-				print 'ERROR: '. $error->getMessage();
-				$mensaje='<div class="alert alert-danger alert-dismissable col-md-offset-4 col-md-3 text-center">
-				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-				<strong>ERROR AL REGISTRAR NUEVO USUARIO</strong></div>';
+				echo "<script> alert('Error al ingresar ');window.location= 'registro.php' </script>";
 				return;
 			}
-			if($stmt)
+			if($stmt1)
 			{
-				header("location: login.php");
-			   $mensaje='<div class="alert alert-success alert-dismissable col-md-offset-4 col-md-3 text-center">
-			   <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-			   <strong>NUEVO USUARIO REGISTRADO CORRECTAMENTE</strong></div>';
-
+				echo "<script> alert('¡Se ingreso correctamente al usuario!');window.location= 'login.php' </script>";
 			}
 		}
 	 }
