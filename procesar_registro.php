6921d87ee1851f6416bf0cfb1f817cbac8cde4ea
Andrey Leal <36980562+alsoza94@users.noreply.github.com>
2018-03-06 01:15:50 -0600

    actualizacion procesar registro encri
    Le agregue metodos de encriptacion al momento de registrar

--- procesar_registro.php
+++ procesar_registro.php
@@ -3,22 +3,25 @@ include('conexion.php');
 /*Si presiono el submit inicia el proceso de registro*/
 if (isset($_POST['registrarse']))
 {
-	$usuario=$_POST['registro_usuario'];
+	 $usuario=$_POST['registro_usuario'];
 	 $contrasena=$_POST['registro_contrasena'];
 	 $contraseña_confirmar=$_POST['confirmar_contrasena'];
 	 $privilegio=$_POST['permisos'];
 
 	 /*Valido que ninguna de las variables este vacia, a pesar de que el mismo formulario ya valida que no vengan vacias*/
-	 if(empty($usuario))
+	 if(empty($_POST['registro_usuario']))
 	 {
+		 echo "<script>alert('Vacio');</script>";
 		 return;
 	 }
-	 else if(empty($contrasena))
+	 else if(empty($_POST['registro_contrasena']))
 	 {
+		 echo "<script>alert('Vacio');</script>";
 		 return;
 	 }
-	 else if(empty($confirmar_contrasena))
+	 else if(empty($_POST['confirmar_contrasena']))
 	 {
+		 echo "<script>alert('Vacio');</script>";
 		 return;
 	 }
 	 else
@@ -28,6 +31,7 @@ if (isset($_POST['registrarse']))
 		$stmt = $conexion->query($query);
 		if ($stmt->rowCount()>0)
 		{
+
 			$mensaje='<div class="alert alert-danger alert-dismissable col-md-offset-4 col-md-3 text-center">
 			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 			Nombre de <strong>Usuario y/o Email</strong> ya existente</div>';
@@ -57,15 +61,19 @@ if (isset($_POST['registrarse']))
 		{
 			try
 			{
+				/*Encripto la contraseña del usuario al momento de insertarse en la BD con password_hash*/
+				$contrasena_cifrada = password_hash($contrasena,PASSWORD_DEFAULT);
+
 				$query="INSERT INTO usuario (usuario, contrasena, privilegio) VALUES (:usuario, :contrasena, :privilegio)";
 				$nuevoUsuario = $conexion -> prepare($query);
 				$nuevoUsuario -> bindParam(':usuario'   , $usuario, PDO::PARAM_STR);
-				$nuevoUsuario -> bindParam(':contrasena', $contrasena, PDO::PARAM_STR);
+				$nuevoUsuario -> bindParam(':contrasena', $contrasena_cifrada, PDO::PARAM_STR);
 				$nuevoUsuario -> bindParam(':privilegio', $privilegio, PDO::PARAM_STR);
 				$stmt= $nuevoUsuario -> execute();
 			}
 			catch (PDOException $error)
 			{
+				echo $error;
 				print 'ERROR: '. $error->getMessage();
 				$mensaje='<div class="alert alert-danger alert-dismissable col-md-offset-4 col-md-3 text-center">
 				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
@@ -74,6 +82,7 @@ if (isset($_POST['registrarse']))
 			}
 			if($stmt)
 			{
+				header("location: login.php");
 			   $mensaje='<div class="alert alert-success alert-dismissable col-md-offset-4 col-md-3 text-center">
 			   <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 			   <strong>NUEVO USUARIO REGISTRADO CORRECTAMENTE</strong></div>';
