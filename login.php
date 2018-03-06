cd9b45f819e96872b8490b05e1ec1c00af83bfba
Andrey Leal <36980562+alsoza94@users.noreply.github.com>
2018-03-06 12:35:39 -0600

    Login
    Actualizacion login

--- login.php
+++ login.php
@@ -5,23 +5,21 @@
 		<title>Inicio de sesión</title>
 	</head>
 	<body>
-		<? echo $mensaje; ?>
-		<form action="procesar_login.php" method="POST" >
+	<form action="procesar_login.php" method="post" >
 			<div class="container">
+				<h2 align="center">Ingresar</h2>
+				<br>
 				<!--Ingreso de usuario-->
 				<label for="uname"><b>Nombre de usuario</b></label>
-				<input type="text" placeholder="Nombre de usuario" name="usuario" id="usuario" required />
-
+				<input type="text" placeholder="Nombre de usuario" name="usuario" id="usuario" required autofocus />
 				<!--Ingreso de contraseña-->
 				<label for="psw"><b>Contraseña</b></label>
 				<input type="password" placeholder="Contraseña" name="contrasena" id="contrasena"  required />
-
 				<!--Boton para inicio de sesion-->
-				<button type="submit" name="submit" >Login</button>
-				<button type="button">Registrarse</button>
-
-				 <span class="loginMsg"><?php echo @$msg;?></span>
-			</div>
+				<button type="submit" name="submit" class="button_aceptar" >Iniciar Sesión</button>
+				<!--Boton para registrarse-->
+				<button type="button" onclick="location.href='registro.php'" class="button_aceptar" >Registrarse</button>
+				</div>
 		</form>
 	</body>
 </hmtl>
